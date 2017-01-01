855def5c75b5325ced590df6841d6416a1a2acd7
temoki <temoki@gmail.com>
2017-01-01 12:07:49 +0900

    Create project

---
+++ OSMX/OSMX/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  OSMX
+//
+//  Created by temoki on 2017/01/01.
+//  Copyright © 2017年 temoki. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
